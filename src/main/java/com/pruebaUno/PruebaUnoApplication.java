937243f623911e7707a224222a4e9a68db4ee155
catta <catta@localhost>
2022-03-10 14:13:30 -0600

    Primer commit

---
+++ src/main/java/com/pruebaUno/PruebaUnoApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaUno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaUnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaUno/PruebaUnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaUno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaUnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
